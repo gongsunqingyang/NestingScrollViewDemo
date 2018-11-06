e3852199c7429f0cfb6185b476f3dc01ac04e25d
杨林 <2851820695@qq.com>
2018-11-06 11:15:00 +0800

    plist

---
+++ UIScrollView嵌套Demo/C/MYScrollViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYScrollViewController.h
+//  UIScrollView嵌套Demo
+//
+//  Created by yanglin on 2018/9/12.
+//  Copyright © 2018年 Softisland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYScrollViewController : UIViewController
+
+@end
---
+++ UIScrollView嵌套Demo/C/MYScrollViewController.m
@@ -0,0 +1,30 @@
+//
+//  MYScrollViewController.m
+//  UIScrollView嵌套Demo
+//
+//  Created by yanglin on 2018/9/12.
+//  Copyright © 2018年 Softisland. All rights reserved.
+//
+
+#import "MYScrollViewController.h"
+
+@interface MYScrollViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
+
+@implementation MYScrollViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
